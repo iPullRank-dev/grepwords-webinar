8f37b065756e8542cf90b24fed9ebff17b1c10c9
John Murch <john@johnmurch.com>
2021-12-16 10:17:44 -0500

    Updates

--- appscript/appscript.js
+++ appscript/appscript.js
@@ -1,9 +1,10 @@
 /*
   1. Create a new Google Sheet
   2. Open Extensions > App Script
-  3. Add new file on left and paste in code
+  3. Replace code and paste in below
   4. Go back to sheet and change a ceil's value to  =search_volume("seo")
-*/
+  5. Bonus: Add a sparkline, adjust the data range but paste into ceil =SPARKLINE(A3:A13,{"charttype","column";"highcolor","red"})
+  */
 
 function search_volume(q) {
   // @CHANGEME - CHANGE BASEURL TO YOUR ENDPOINT
